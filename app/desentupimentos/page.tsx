48c66df8e4b72476ea1672d08683643f3039aac2
Yuri Almeida <73500564+yalmeidarj@users.noreply.github.com>
2023-09-05 09:30:38 -0400

    SEO setup

--- app/desentupimentos/page.tsx
+++ app/desentupimentos/page.tsx
@@ -24,23 +24,23 @@ export async function generateMetadata() {
     }
 
     return metadata
+}
 
 
+export default function Page() {
+    return (
+        <div className="min-h-screen bg-white text-gray-dark p-4 md:p-8">
+            <h1 className="text-2xl md:text-4xl font-bold mb-4 md:mb-8">Nossos Serviços</h1>
+            {/* <h2 className="text-xl md:text-2xl font-bold mb-4 md:mb-8">Desentupimento 24 horas em todo Rio de Janeiro</h2> */}
+            {/* Make subtitle even smaller   */}
+            <h3 className="text-sm md:text-xl font-bold mb-4 md:mb-8">Desentupimento 24 horas em todo Rio de Janeiro</h3>
+            <p className="text-black">Desentupimento de ralos, pias e bueiros. Se você está enfrentando problemas de entupimento, temos a solução.</p>
+            <WhatsAppButton />
 
-    export default function Page() {
-        return (
-            <div className="min-h-screen bg-white text-gray-dark p-4 md:p-8">
-                <h1 className="text-2xl md:text-4xl font-bold mb-4 md:mb-8">Nossos Serviços</h1>
-                {/* <h2 className="text-xl md:text-2xl font-bold mb-4 md:mb-8">Desentupimento 24 horas em todo Rio de Janeiro</h2> */}
-                {/* Make subtitle even smaller   */}
-                <h3 className="text-sm md:text-xl font-bold mb-4 md:mb-8">Desentupimento 24 horas em todo Rio de Janeiro</h3>
-                <p className="text-black">Desentupimento de ralos, pias e bueiros. Se você está enfrentando problemas de entupimento, temos a solução.</p>
-                <WhatsAppButton />
+            <ul className="space-y-4">
 
-                <ul className="space-y-4">
+            </ul>
+        </div>
 
-                </ul>
-            </div>
-
-        );
-    }
+    );
+}
