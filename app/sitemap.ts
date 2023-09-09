28b4271077a9e6856dfedca304bed3db8ae7ac88
Yuri Almeida <73500564+yalmeidarj@users.noreply.github.com>
2023-09-09 06:10:11 -0400

    sitemap update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -12,6 +12,44 @@ export default async function sitemap() {
     { url: baseUrl, lastModified: new Date() },
     { url: `${baseUrl}/sobre`, lastModified: new Date() },
     { url: `${baseUrl}/desentupimentos`, lastModified: new Date() },
+    // { desentupimentos comerciais }
+    {
+      url: `${baseUrl}/desentupimentos/comercial/caixa-de-gordura`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/desentupimentos/comercial/coluna`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/desentupimentos/comercial/contrato-de-manutencao`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/desentupimentos/comercial/manutencao-de-esgoto`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/desentupimentos/comercial/pias-vasos-sanitarios-ralos`,
+      lastModified: new Date(),
+    },
+    // { desentupimentos residenciais }
+    {
+      url: `${baseUrl}/desentupimentos/residencial/caixa-de-gordura`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/desentupimentos/residencial/coluna`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/desentupimentos/residencial/esgoto`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/desentupimentos/residencial/pias-vasos-sanitarios-ralos`,
+      lastModified: new Date(),
+    },
     { url: `${baseUrl}/blog`, lastModified: new Date() },
 
     ...paths,
